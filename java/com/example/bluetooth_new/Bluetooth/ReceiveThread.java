304962127ffc53138c0b2d16efa91f91c1134bc9
artemlukichev <85209906+artemlukichev@users.noreply.github.com>
2021-08-26 13:51:13 +0300

    Update ReceiveThread.java
    Add data received

--- java/com/example/bluetooth_new/Bluetooth/ReceiveThread.java
+++ java/com/example/bluetooth_new/Bluetooth/ReceiveThread.java
@@ -1,4 +1,6 @@
-package com.example.bluetooth_new.Bluetooth;
+package com.example.ble_app.Bluetooth;
+
+import static com.example.ble_app.adapter.BtConsts.MAC_KEY;
 
 import android.bluetooth.BluetoothSocket;
 import android.util.Log;
@@ -24,15 +26,33 @@ public class ReceiveThread extends  Thread {
 
     @Override
     public void run() {
+        byte[] value = Utils.VALUE_CLOCK_TIME;
+        Utils.sendValueToBle(MAC_KEY, Utils.UUID_TICD, Utils.UUID_TCID_CH, value);
         rBuffer = new byte[1000];
         while (true){
             try{
-            int size = inputS.read(rBuffer); //считываем в массив
+                int size = inputS.read(rBuffer); //считываем в массив
                 String message = new String(rBuffer,0,size);
+                onDataRceived(value);
                 Log.d("MyLog","Message: "+ message); //что получаем из устройства
             } catch (IOException e){ //если срыв соединения
                 break;
             }
+        }
+
+    }
+    protected void onDataRceived(byte [] array)
+    {
+        if (5 == array.length && 0x23 == array[0])
+        {
+            byte first = array[1];
+            byte second = array[2];
+
+            int y = (first  & 0xFF) >> 1;
+            int m1 = (first & 0x1) << 3;
+            int m2 = (second & 0xFF) >> 5;
+            int m = m1 + m2;
+            int d = second & 0x1f;
         }
     }
 }

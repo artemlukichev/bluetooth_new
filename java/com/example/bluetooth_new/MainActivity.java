d888d6a40bd287fe8025d151d6368ae2ef43c57a
artemlukichev <85209906+artemlukichev@users.noreply.github.com>
2021-09-01 16:01:54 +0300

    Add buttons for pr.

--- java/com/example/bluetooth_new/MainActivity.java
+++ java/com/example/bluetooth_new/MainActivity.java
@@ -25,13 +25,65 @@ public class MainActivity extends AppCompatActivity {
     private final int ENABLE_REQUEST = 15;
     private SharedPreferences pref;
     private BtConnection btConnection;
+    private Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+        b5 = findViewById(R.id.b5);
+        b6 = findViewById(R.id.b6);
+        b7 = findViewById(R.id.b7);
+        b8 = findViewById(R.id.b8);
+        b9 = findViewById(R.id.b9);
+        b10 = findViewById(R.id.b10);
+        b11 = findViewById(R.id.b11);
+        b12 = findViewById(R.id.b12);
         init();
+
+        //Слушатель кнопок
+        b1.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_CLOCK_TIME);
+        });
+        b2.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_MODEL);
+        });
+        b3.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_READ_STORAGE_DATA_P1);
+        });
+        b4.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_READ_STORAGE_DATA_P2);
+        });
+        b5.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_SERIAL_NUMBER_P1);
+        });
+        b6.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_SERIAL_NUMBER_P2 );
+        });
+        b7.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_STORAGE_NUM_OF_DATA);
+        });
+        b8.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_DEVICE_CLOCK_TIME);
+        });
+        b9.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_START_MEASUREMENT);
+        });
+        b10.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_TURN_OFF);
+        });
+        b11.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_CLEAR_DEL_MEMORY);
+        });
+        b12.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_NOTIFICATION);
+        });
+    }
     }
 
     //в меню создаем рразметку

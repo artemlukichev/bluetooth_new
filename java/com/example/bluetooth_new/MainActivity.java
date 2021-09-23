40538ed8222516d46537c97b7015d3e2e12b0c5d
artemlukichev <85209906+artemlukichev@users.noreply.github.com>
2021-09-23 15:22:23 +0300

    Update MainActivity.java

--- java/com/example/bluetooth_new/MainActivity.java
+++ java/com/example/bluetooth_new/MainActivity.java
@@ -1,92 +1,83 @@
-package com.example.bluetooth_new;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.ble_app;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.bluetooth_new.Bluetooth.BtConnection;
-import com.example.bluetooth_new.adapter.BtConsts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ble_app.Bluetooth.BtConnection;
+import com.example.ble_app.Bluetooth.ReceiveThread;
+import com.example.ble_app.Bluetooth.Utils;
+import com.example.ble_app.adapter.BtConsts;
+import com.example.bluetooth_new.R;
+
+import java.text.CollationElementIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     private MenuItem menuItem;
     private BluetoothAdapter btAdapter;
     private final int ENABLE_REQUEST = 15;
     private SharedPreferences pref;
     private BtConnection btConnection;
-    private Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
+    private Button options, start, result;
+    public TextView textView3;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = findViewById(R.id.b1);
-        b2 = findViewById(R.id.b2);
-        b3 = findViewById(R.id.b3);
-        b4 = findViewById(R.id.b4);
-        b5 = findViewById(R.id.b5);
-        b6 = findViewById(R.id.b6);
-        b7 = findViewById(R.id.b7);
-        b8 = findViewById(R.id.b8);
-        b9 = findViewById(R.id.b9);
-        b10 = findViewById(R.id.b10);
-        b11 = findViewById(R.id.b11);
-        b12 = findViewById(R.id.b12);
-        init();
+        options = (Button) findViewById(R.id.options);
+        result = findViewById(R.id.result);
+        textView3 = findViewById(R.id.textView3);
+        start= findViewById(R.id.start);
+        //сделать в мейнактиви глобальную переменную
+        ReceiveThread.a = this; // присв. из ресива
+        ReceiveThread.b = this;
 
+
+        init();
         //Слушатель кнопок
-        b1.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_CLOCK_TIME);
+        options.setOnClickListener(v -> {
+            Intent y = new Intent(MainActivity.this, OptionsActivity.class);
+            startActivity(y);
         });
-        b2.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_MODEL);
-        });
-        b3.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_READ_STORAGE_DATA_P1);
-        });
-        b4.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_READ_STORAGE_DATA_P2);
-        });
-        b5.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_SERIAL_NUMBER_P1);
-        });
-        b6.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_SERIAL_NUMBER_P2 );
-        });
-        b7.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_STORAGE_NUM_OF_DATA);
-        });
-        b8.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_DEVICE_CLOCK_TIME);
-        });
-        b9.setOnClickListener(v -> {
+
+        
+
+
+        start.setOnClickListener(v -> {
             btConnection.sendMessage(Utils.VALUE_START_MEASUREMENT);
         });
-        b10.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_TURN_OFF);
+
+        result.setOnClickListener(v -> {
+            btConnection.sendMessage(Utils.VALUE_READ_STORAGE_DATA_P2);
         });
-        b11.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_CLEAR_DEL_MEMORY);
-        });
-        b12.setOnClickListener(v -> {
-            btConnection.sendMessage(Utils.VALUE_NOTIFICATION);
-        });
-    }
+
     }
 
-    //в меню создаем рразметку
+
+
+
+    //в меню разметка
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -96,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
 
     //прослушиваем нажатие кнопки
     @Override
@@ -167,4 +159,5 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(i, ENABLE_REQUEST);
 
     }
+
 }
